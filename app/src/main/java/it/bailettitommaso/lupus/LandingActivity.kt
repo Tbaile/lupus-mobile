284181387f8b05d463482fba5d1c399b42db2029
Tommaso Bailetti <bailetti.tommaso@gmail.com>
2022-08-31 16:52:29 +0200

    Renamed activity to landing

--- app/src/main/java/it/bailettitommaso/lupus/LandingActivity.kt
+++ app/src/main/java/it/bailettitommaso/lupus/LandingActivity.kt
@@ -3,9 +3,10 @@ package it.bailettitommaso.lupus
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class LandingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setTheme(R.style.Theme_Lupus_NoActionBar)
+        setContentView(R.layout.landing_activity)
     }
 }
