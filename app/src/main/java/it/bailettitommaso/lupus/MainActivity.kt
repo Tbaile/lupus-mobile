347c4f9873dc94ac2485325d9c9d97d78e2c6272
Tommaso Bailetti <bailetti.tommaso@gmail.com>
2022-10-14 17:28:55 +0200

    Added new MainActivity

---
+++ app/src/main/java/it/bailettitommaso/lupus/MainActivity.kt
@@ -0,0 +1,7 @@
+package it.bailettitommaso.lupus
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(R.layout.main_activity)
