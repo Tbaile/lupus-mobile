dd56a498f7cf5f8b1b2a1b9816d9db36bcdebd94
Tommaso Bailetti <bailetti.tommaso@gmail.com>
2022-10-17 15:24:22 +0200

    Removed LandingActivity, added LandingFragment
    Added layouts and navigations

---
+++ app/src/main/java/it/bailettitommaso/lupus/LandingFragment.kt
@@ -0,0 +1,39 @@
+package it.bailettitommaso.lupus
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import dagger.hilt.android.AndroidEntryPoint
+import it.bailettitommaso.lupus.models.Resource
+import it.bailettitommaso.lupus.viewmodels.SelfUserViewModel
+
+@AndroidEntryPoint
+class LandingFragment : Fragment(R.layout.fragment_landing) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val selfUserViewModel: SelfUserViewModel by viewModels()
+        selfUserViewModel.getSelfUser().observe(viewLifecycleOwner) {
+            when (it) {
+                is Resource.Error -> {
+                    val error: String = if (it.statusCode == 401) {
+                        "Login expired, please login again."
+                    } else {
+                        it.message!!
+                    }
+                    Toast.makeText(context, error, Toast.LENGTH_LONG).show()
+                    findNavController().navigate(R.id.action_login_required)
+                }
+                is Resource.Loading -> {
+                    Log.d("LupusTest", "Loading...")
+                }
+                else -> {
+                    Log.d("LupusTest", it.data.toString())
+                }
+            }
+        }
+    }
+}
